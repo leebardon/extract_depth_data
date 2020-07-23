57e0557936e6997e9d215b543c8317842d26ffb1
github username <github email address>
2020-07-23 09:34:27 +0100

    minor alteration of core function

--- get_depth/get_depth.py
+++ get_depth/get_depth.py
@@ -15,7 +15,7 @@ df = ds.to_dataframe()
 
 def get_depth(data, month, depth):
     surface = []
-    for i in range(0, len(data)):
+    for i in range(0, 285120):
         if df.iloc[i].name[3] == depth:
             df["X"] = df.iloc[i].name[1]
             df["Y"] = df.iloc[i].name[2]
